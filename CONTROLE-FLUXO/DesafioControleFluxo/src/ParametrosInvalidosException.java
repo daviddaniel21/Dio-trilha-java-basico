b5cdc020935f62e809d3ca33226f7618c4867aea
daviddaniel21 <dandias987@gmail.com>
2023-09-05 19:50:21 -0300

    Desafio controle de fluxo

---
+++ CONTROLE-FLUXO/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException() {
+        super("Parâmetros inválidos.");
+    }
+
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
